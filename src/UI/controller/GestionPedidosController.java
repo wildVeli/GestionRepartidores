190e1dd5d6f44c2a11cc29d2f06916bb678aa49e
ubuntu <ubuntu@ubuntu>
2017-11-06 14:26:14 +0100

    6-11-17

--- src/UI/controller/GestionPedidosController.java
+++ src/UI/controller/GestionPedidosController.java
@@ -8,14 +8,22 @@ package UI.controller;
 import control.PedidoBean;
 import control.PedidosManager;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -45,6 +53,14 @@ public class GestionPedidosController  {
     private TableColumn tbcolnRepartidor;
     @FXML
     private TableView<PedidoBean> tablaPedidos;
+    @FXML
+    private Button detalles;
+    @FXML
+    private Button eliminar;
+    @FXML
+    private Button salir;
+    @FXML
+    private Button nuevoPedido;
     private PedidosManager pedidosManager;
     
     
@@ -79,6 +95,33 @@ public class GestionPedidosController  {
             
         }     
         tablaPedidos.setItems(pedidosData);
+        //Añade un listener para reaccinar a la selección de filas de una tabla
+        tablaPedidos.getSelectionModel().selectedIndexProperty().addListener(this::handlePeidosTableSelectionChanged);
+    }
+    private void handlePeidosTableSelectionChanged(ObservableValue observable,Object oldValue,Object newValue){
+        //Si se ha seleccionado una fila de la tabla,
+        //Habilitar los botones de detalles y eliminar
+        if(newValue!=null){
+            PedidoBean pedido=(PedidoBean)newValue;
+            detalles.setDisable(false);
+            eliminar.setDisable(false);
+        }
+    }
+    @FXML
+    private void handleBotonDetallesAction(ActionEvent event){
+        
+    }
+    @FXML
+    private void handleBotonEliminarAction(ActionEvent event){
+        Alert alert = new Alert(AlertType.CONFIRMATION,"¿Está seguro de que desea eliminar el pedido? ");
+        DialogPane dialogPane = alert.getDialogPane();
+        alert.setHeaderText("Confirmación");
+        alert.setContentText("Este cambio será irreversible");
+        //dialogPane.getStylesheets().add(getClass().getResource("Custom.css").toExternalForm());
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get()==ButtonType.OK){
+            tablaPedidos.getItems().remove(tablaPedidos.getSelectionModel().getSelectedItem());
+        }
         
     }
     
